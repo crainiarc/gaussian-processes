8f233c6c2afb526f314a03f9aef587ec741d0ec2
Keng Kiat Lim <kkin@crnrc.com>
2015-01-18 23:24:43 +0800

    Added sparse_pitc files

---
+++ src/sparse_pitc.cpp
@@ -0,0 +1,9 @@
+//
+//  sparse_pitc.cpp
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 18/1/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#include "sparse_pitc.h"
---
+++ src/sparse_pitc.h
@@ -0,0 +1,14 @@
+//
+//  sparse_pitc.h
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 18/1/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#ifndef __GaussianProcess__sparse_pitc__
+#define __GaussianProcess__sparse_pitc__
+
+#include <stdio.h>
+
+#endif /* defined(__GaussianProcess__sparse_pitc__) */
