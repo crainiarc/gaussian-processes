a1f760a789710ade51999081fd4716e383b97954
Keng Kiat Lim <kkin@crnrc.com>
2015-01-20 17:57:48 +0800

    Added method signatures for sparse pitc

--- src/sparse_pitc.cpp
+++ src/sparse_pitc.cpp
@@ -7,3 +7,27 @@
 //
 
 #include "sparse_pitc.h"
+
+PITCSparseGP::PITCSparseGP(kernel_func_t kernelFunc, double noiseVar, int blockSize) :
+    GaussianProcess(kernelFunc, noiseVar), mBlockSize(blockSize) {
+}
+
+PITCSparseGP::PITCSparseGP(kernel_func_t kernelFunc, double noiseVar, int blockSize, bool autoLearn) :
+    GaussianProcess(kernelFunc, noiseVar, autoLearn), mBlockSize(blockSize) {
+}
+
+auto learn() -> void {
+    
+}
+
+auto predict(const arma::Mat<double> &testData) -> std::tuple<arma::Mat<double>, arma::Mat<double>> {
+    return arma::Mat<double>();
+}
+
+auto predictMean(const arma::Mat<double> &testData) -> arma::Mat<double>  {
+    return arma::Mat<double>();
+}
+
+auto predictVariance(const arma::Mat<double> &testData) -> arma::Mat<double> {
+    return arma::Mat<double>();
+}
--- src/sparse_pitc.h
+++ src/sparse_pitc.h
@@ -9,6 +9,21 @@
 #ifndef __GaussianProcess__sparse_pitc__
 #define __GaussianProcess__sparse_pitc__
 
-#include <stdio.h>
+#include <armadillo>
+#include "gp.h"
+
+class PITCSparseGP : public GaussianProcess {
+    friend class PITCSparseGPTest;
+    
+public:
+    PITCSparseGP(kernel_func_t kernelFunc, double noiseVar, int blockSize);
+    PITCSparseGP(kernel_func_t kernelFuct, double noiseVar, int blockSize, bool autoLearn);
+    auto setInducingInputs(const arma::Mat<double> &inducingInputs) -> void;
+    
+protected:
+    int mBlockSize;
+    arma::Mat<double> mInducingInputs;
+    arma::Mat<double> mInducingCovariances;
+};
 
 #endif /* defined(__GaussianProcess__sparse_pitc__) */
