9a182ea44c2925b585daa99ffc092a581d3885ff
Keng Kiat Lim <kkin@crnrc.com>
2015-03-08 04:55:32 +0800

    Added method stubs for MultiOutputOnlinePITCGP

--- src/multi_online_pitc_gp.cpp
+++ src/multi_online_pitc_gp.cpp
@@ -7,3 +7,60 @@
 //
 
 #include "multi_online_pitc_gp.h"
+
+MultiOutputOnlinePITCGP::MultiOutputOnlinePITCGP(int blockSize, arma::Mat<double> latentVars, MultiOutputHyperparameters hypers) :
+    mBlockSize(blockSize), mLatentVariables(latentVars), mHyperparameters(hypers)
+{
+    autoLearn = true;
+}
+
+MultiOutputOnlinePITCGP::MultiOutputOnlinePITCGP(int blockSize, arma::Mat<double> latentVars, MultiOutputHyperparameters hypers, bool autoLearn) :
+    mBlockSize(blockSize), mLatentVariables(latentVars), mHyperparameters(hypers), mAutoLearn(autoLearn)
+{
+}
+
+auto MultiOutputOnlinePITCGP::setTrainingSet(const arma::Mat<double> &data, const arma::Mat<double> &obs) {
+}
+
+auto MultiOutputOnlinePITCGP::addTrainingSet(const arma::Mat<double> &data, const arma::Mat<double> &obs) {
+}
+
+auto MultiOutputOnlinePITCGP::setHyperParameters(MultiOutputHyperparameters hypers) {
+}
+
+auto MultiOutputOnlinePITCGP::setAutoLearn(bool autoLearn) {
+}
+
+auto MultiOutputOnlinePITCGP::learn() {
+}
+
+auto MultiOutputOnlinePITCGP::predict(const arma::Mat<double> &testData) -> std::tuple<arma::Mat<double>, arma::Mat<double>> {
+    return std::make_tuple(arma::Mat<double>, arma::Mat<double>);
+}
+
+auto MultiOutputOnlinePITCGP::predictMean(const arma::Mat<double> &testData) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
+
+auto MultiOutputOnlinePITCGP::predictVariance(const arma::Mat<double> &testData) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
+
+auto MultiOutputOnlinePITCGP::initHyperparameters() {
+}
+
+auto MultiOutputOnlinePITCGP::computeKff(arma::Mat<double> X, int q) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
+
+auto MultiOutputOnlinePITCGP::computeKfu(arma::Mat<double> X, int q) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
+
+auto MultiOutputOnlinePITCGP::computeKuu(arma::Mat<double> X) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
+
+auto MultiOutputOnlinePITCGP::computeKtf(arma::Mat<double> X_star, arma::Mat<double> X, int q) -> arma::Mat<double> {
+    return arma::Mat<double>;
+}
