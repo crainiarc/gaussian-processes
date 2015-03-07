5032a88835bc9cd0d3c17cc70ef5a223a9011850
Keng Kiat Lim <kkin@crnrc.com>
2015-03-08 04:55:10 +0800

    Added class definitions for MultiOutputOnlinePITCGP

--- src/multi_online_pitc_gp.h
+++ src/multi_online_pitc_gp.h
@@ -9,6 +9,52 @@
 #ifndef __GaussianProcess__multi_online_pitc_gp__
 #define __GaussianProcess__multi_online_pitc_gp__
 
-#include <stdio.h>
+#include <armadillo>
+
+struct MultiOutputHyperparameters {
+    arma::Col<double> biases;
+    arma::Col<double> scales;
+    arma::Col<double> betas;
+    
+    arma::Col<double> variances;
+    arma::Col<double> sigma2Ys;
+    arma::Col<double> sigma2Us;
+    
+    arma::Mat<double> precisionYs;
+    arma::Mat<double> precisionUs;
+};
+
+
+class MultiOutputOnlinePITCGP {
+    friend class MultiOutputOnlinePITCGPTest;
+    
+public:
+    MultiOutputOnlinePITCGP(int blockSize, arma::Mat<double> latentVars, MultiOutputHyperparameters hypers);
+    MultiOutputOnlinePITCGP(int blockSize, arma::Mat<double> latentVars, MultiOutputHyperparameters hypers, bool autoLearn);
+    
+    auto setTrainingSet(const arma::Mat<double> &data, const arma::Mat<double> &obs) -> void;
+    auto addTrainingSet(const arma::Mat<double> &data, const arma::Mat<double> &obs) -> void;
+    auto setHyperParameters(MultiOutputHyperparameters hypers) -> void;
+    auto setAutoLearn(bool autoLearn) -> void;
+    
+    auto learn() -> void;
+    auto predict(const arma::Mat<double> &testData) -> std::tuple<arma::Mat<double>, arma::Mat<double>>;
+    auto predictMean(const arma::Mat<double> &testData) -> arma::Mat<double>;
+    auto predictVariance(const arma::Mat<double> &testData) -> arma::Mat<double>;
+    
+protected:
+    int mBlockSize;
+    bool mAutoLearn;
+    arma::Mat<double> mTrainingSet;
+    arma::Mat<double> mObservations;
+    arma::Mat<double> mLatentVariables;
+    MultiOutputHyperparameters mHyperparameters;
+    
+    auto initHyperparameters() -> void;
+    auto computeKff(arma::Mat<double> X, int q) -> arma::Mat<double>;
+    auto computeKfu(arma::Mat<double> X, int q) -> arma::Mat<double>;
+    auto computeKuu(arma::Mat<double> X) -> arma::Mat<double>;
+    auto computeKtf(arma::Mat<double> X_star, arma::Mat<double> X, int q) -> arma::Mat<double>;
+};
 
 #endif /* defined(__GaussianProcess__multi_online_pitc_gp__) */
