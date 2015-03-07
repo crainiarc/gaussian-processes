370c904eaae1a13bb9f222adc89bd6dd872187d8
Keng Kiat Lim <kkin@crnrc.com>
2015-03-08 04:54:45 +0800

    Added multi_online_pitc_gp files

---
+++ src/multi_online_pitc_gp.cpp
@@ -0,0 +1,9 @@
+//
+//  multi_online_pitc_gp.cpp
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 8/3/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#include "multi_online_pitc_gp.h"
---
+++ src/multi_online_pitc_gp.h
@@ -0,0 +1,14 @@
+//
+//  multi_online_pitc_gp.h
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 8/3/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#ifndef __GaussianProcess__multi_online_pitc_gp__
+#define __GaussianProcess__multi_online_pitc_gp__
+
+#include <stdio.h>
+
+#endif /* defined(__GaussianProcess__multi_online_pitc_gp__) */
