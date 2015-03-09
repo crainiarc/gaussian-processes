7ef748ff0459df7cdd2a3c65b2987ee59cb79328
Keng Kiat Lim <kkin@crnrc.com>
2015-03-09 21:51:54 +0800

    Wrote tests to test the reciprocal operation in armadillo

---
+++ multi_online_pitc_gp_test.cpp
@@ -0,0 +1,26 @@
+//
+//  multi_online_pitc_gp.cpp
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 9/3/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#include <armadillo>
+#include "gtest/gtest.h"
+#include "multi_online_pitc_gp.h"
+
+TEST(BasicOperations, InverseReciprocalTest) {
+    arma::Mat<double> mat = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    mat.reshape(3, 3);
+    arma::Mat<double> inv = 1.0 / mat;
+    
+    ASSERT_EQ(mat.n_rows, inv.n_rows);
+    ASSERT_EQ(mat.n_cols, inv.n_cols);
+    
+    for (unsigned int row = 0; row < mat.n_rows; ++row) {
+        for (unsigned int col = 0; col < mat.n_cols; ++col) {
+            ASSERT_FLOAT_EQ(1.0 / mat(row, col), inv(row, col));
+        }
+    }
+}
