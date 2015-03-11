bd210f0bd1711699de5000c31194eb65fb6af6f8
Keng Kiat Lim <kkin@crnrc.com>
2015-03-12 00:35:52 +0800

    Added main.cpp for MOGPExample

---
+++ mogp-example/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  MOGPExample
+//
+//  Created by Keng Kiat Lim on 10/3/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#include <iostream>
+#include "../gaussian_processes/multi_output.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
