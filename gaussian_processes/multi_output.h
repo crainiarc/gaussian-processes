7eda03aeb60d727ca338ee73d6e619f663787903
Keng Kiat Lim <kkin@crnrc.com>
2015-03-12 00:33:19 +0800

    Added multi_output.h

---
+++ gaussian_processes/multi_output.h
@@ -0,0 +1,14 @@
+//
+//  multi_output.h
+//  GaussianProcess
+//
+//  Created by Keng Kiat Lim on 12/3/15.
+//  Copyright (c) 2015 Keng Kiat Lim. All rights reserved.
+//
+
+#ifndef GaussianProcess_multi_output_h
+#define GaussianProcess_multi_output_h
+
+#include "multi_output/online_pitc.h"
+
+#endif
